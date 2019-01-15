f215c40b89f17ce54bbdd119d77695b402757a70
duaa.zahi@altayyargroup.com <7W9y4voaCzq9>
2019-01-15 15:45:11 +0400

    add simple project files

---
+++ hello.js
@@ -0,0 +1,6 @@
+re('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis!\n') 
+}).listen(process.env.PORT || 3000);
+console.log('Server running at http://localhost:' + app.server.address().port);
