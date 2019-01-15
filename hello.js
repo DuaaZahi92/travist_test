dfb94e5b295cb4d1c4af23d601891adbbb3d6869
duaa.zahi@altayyargroup.com <7W9y4voaCzq9>
2019-01-15 17:21:15 +0400

    fix syntax error

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 re('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis!\n') 
+  res.end('Hello Travis!\n'); 
 }).listen(process.env.PORT || 3000);
 
 console.log('Server running at http://localhost:' + app.server.address().port);
