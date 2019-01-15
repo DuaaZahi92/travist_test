b27a63909303c9211ff2740d560076a45814af0e
duaa.zahi@altayyargroup.com <7W9y4voaCzq9>
2019-01-15 17:26:03 +0400

    add notifications

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 re('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+ ; res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n'); 
 }).listen(process.env.PORT || 3000);
 
